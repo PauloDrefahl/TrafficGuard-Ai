4ad6efd8c893925da9afa3cefe45e08c9204d1e0
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2025-04-14 13:05:35 -0400

    NavBar code restrcutures & added comments

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,16 +3,25 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
 function CollapsibleExample() {
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+
       <Container>
+
+        // Nav Title 
         <Navbar.Brand href="#home">🏠 FrED IoT Home System</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
+
+          // Nav Options
           <Nav className="me-auto">
             <Nav.Link href="#iot-devices">IoT Devices</Nav.Link>
+            
             <Nav.Link href="#network-status">Network Status</Nav.Link>
+
             <NavDropdown title="More Options" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#settings">Settings</NavDropdown.Item>
               <NavDropdown.Item href="#activity-logs">Activity Logs</NavDropdown.Item>
@@ -21,10 +30,12 @@ function CollapsibleExample() {
               <NavDropdown.Item href="#support">Support</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+
           <Nav>
             <Nav.Link href="#notifications">Notifications</Nav.Link>
             <Nav.Link eventKey={2} href="#account">Exit ↪</Nav.Link>
           </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
