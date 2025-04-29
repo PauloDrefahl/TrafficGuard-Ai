e669dbbe016403215dd7fd1746eadfc9e6345ece
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2025-04-29 17:39:57 -0400

    added drop down menus to the changerouter file

--- frontend/src/components/ChangeRouter.jsx
+++ frontend/src/components/ChangeRouter.jsx
@@ -1,8 +1,29 @@
 // Change Router Card on the Settings PAGE
-
+import React, { useState } from 'react';
 import ScrollBoxRouter from './ScrollBoxRouter';
 
 const ChangeRouter = () => {
+
+    // Brand Dropdown Menu .js
+    const brands = ['Mango', 'Beryl'];
+    const [selectedBrand, setSelectedBrand] = useState('-- Select --');
+    const [isBrandOpen, setIsBrandOpen] = useState(false);
+
+    const handleBrandClick = (brand) => {
+        setSelectedBrand(brand);
+        setIsBrandOpen(false);
+    };
+
+    // Model Dropdown Menu .js
+    const models = ['Mango', 'Beryl'];
+    const [selectedModel, setSelectedModel] = useState('-- Select --');
+    const [isModelOpen, setIsModelOpen] = useState(false);
+
+    const handleModelClick = (model) => {
+        setSelectedModel(model);
+        setIsModelOpen(false);
+    };
+
 
     return(
 
@@ -51,26 +72,106 @@ const ChangeRouter = () => {
                 </div>
 
 
+
                 {/* Add Router Section */}
                 <h11>Add Router</h11>
-                <h10 style={{ marginLeft: '25px'}}>Router Brand </h10>
 
-
-                <h10 style={{ marginLeft: '25px'}}>Router Model </h10>
-
-
-                
+                {/* Router Brand Menu */}
                 <div 
                     style={{
                         display: 'flex', 
                         flexDirection: 'row', 
                         alignItems: 'center', 
-                        gap: '55px'
+                        gap: '25px'
+                    }}>
+
+                    <h10 style={{ marginLeft: '25px'}}>Router Brand</h10>
+
+                    <div className="dropdown">
+
+                        <button className="dropbtn" onClick={() => setIsBrandOpen(!isBrandOpen)}>
+                            {selectedBrand} <span className="arrow">{isBrandOpen ? '▲' : '▼'}</span>
+                        </button>
+
+                        {isBrandOpen && (
+
+                            <div className="dropdown-content">
+
+                            {brands.map((brands, idx) => (
+
+                                <div
+
+                                key={idx}
+                                className="dropdown-item"
+                                onClick={() => handleBrandClick(brands)}
+                                >
+
+                                {brands}
+
+                                </div>
+                            ))}
+
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {/* Router Model Menu */}
+                <div 
+                    style={{
+                        display: 'flex', 
+                        flexDirection: 'row', 
+                        alignItems: 'center', 
+                        gap: '22px',
+                        position: 'relative', /* KEY */
+                        overflow: 'visible' /* ensure dropdown can spill over */
+                    }}>
+
+                    <h10 style={{ marginLeft: '25px'}}>Router Model </h10>
+
+                    <div className="dropdown" style={{ position: 'relative' }}>
+
+                        <button className="dropbtn" onClick={() => setIsModelOpen(!isModelOpen)}>
+
+                            {selectedModel} <span className="arrow">{isModelOpen ? '▲' : '▼'}</span>
+
+                        </button>
+
+                        {isModelOpen && (
+
+                            <div className="dropdown-content">
+
+                            {models.map((model, idx) => (
+
+                                <div
+
+                                key={idx}
+                                className="dropdown-item"
+                                onClick={() => handleModelClick(model)}
+                                >
+
+                                {model}
+
+                                </div>
+                            ))}
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+                
+                 {/* User IP Address Text Box */}
+                <div 
+                    style={{
+                        display: 'flex', 
+                        flexDirection: 'row', 
+                        alignItems: 'center', 
+                        gap: '50px'
                     }}>
                     
                     <h10 style={{ marginLeft: '25px'}}>IP Address </h10>
                     {/* User Ip Textbox */}
-                    <div class="text-box"
+                    <div className="text-box"
 
                         style={{
 
@@ -79,7 +180,7 @@ const ChangeRouter = () => {
 
                         }}>
 
-                    <input type="text" class="custom-input" placeholder="e.g. 192.103.577.901"/>
+                    <input type="text" className="custom-input" placeholder="e.g. 192.103.577.901"/>
 
                     </div>
 
