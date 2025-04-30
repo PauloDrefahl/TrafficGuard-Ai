8de45b5050f1355112168e1bc7b2ece5af0f5f0c
Alyssa Chiego <abmccurdy7151@eagle.fgcu.edu>
2025-04-29 20:23:07 -0400

    updated (temp) navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,33 @@
-// Navigation Bar
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
-
   return (
-    
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
 
-        {/* Nav Title */}
-        <h9>FrED IoT Home System</h9>
-             
-        <Link to="/">Home</Link>
-        <Link to="/NetworkStatus">Network Status</Link>
-        <Link to="/Notifications">Notifications</Link>
-        <Link to="/Settings">Settings</Link>
+        {/* Brand / Title */}
+        <Navbar.Brand as={Link} to="/">FrED IoT Home System</Navbar.Brand>
 
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+
+          {/* Left-side Navigation Links */}
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/NetworkStatus">Network Status</Nav.Link>
+            <Nav.Link as={Link} to="/Notifications">Notifications</Nav.Link>
+            <Nav.Link as={Link} to="/Settings">Settings</Nav.Link>
+          </Nav>
 
           {/* Right-side Options */}
           <Nav>
-          
-            <Nav.Link eventKey={2} href="#account">Exit ↪</Nav.Link>
-
+            <Nav.Link href="#account">Exit ↪</Nav.Link>
           </Nav>
 
-        
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
